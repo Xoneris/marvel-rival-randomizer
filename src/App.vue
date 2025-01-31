02b678db6d3b57727c5050d5e8f13a204b727e30
Xoneris <kely42@gmx.ch>
2025-01-31 20:45:53 +0100

    added button for each role + bugfixes.

--- src/App.vue
+++ src/App.vue
@@ -16,55 +16,78 @@
 
   const lastHeroes = ref<Hero[]>([])
 
-  function randomHero() {
+  function randomHero(type:string) {
 
-    if(currentHero.value.Hero !== "") {
-      if(lastHeroes.value.length === 3) {
-        lastHeroes.value.reverse().pop()
-        lastHeroes.value.reverse().push({...currentHero.value})
-      } else {
-        lastHeroes.value.push({...currentHero.value})
-      }
+    let heroArray
+
+    if (type !== "") {
+      heroArray = heroes.filter((hero) => hero.Type === type)
+    } else {
+      heroArray = heroes
     }
 
-    
-
-    let random = Math.floor(Math.random() * heroes.length)
-
-    for(let i=0 ; i<lastHeroes.value.length ; i++) {
-      let ok = false
-      while(ok === false) {
-        if (heroes[random].Hero === lastHeroes.value[i].Hero) {
-          random = Math.floor(Math.random() * heroes.length)
-        } else {
-          ok = true
-        }
+    if (currentHero.value.Hero !== "") {
+      if (lastHeroes.value.length >= 3) {
+        lastHeroes.value.shift();
       }
+      lastHeroes.value.push({ ...currentHero.value });
     }
 
-    currentHero.value = heroes[random]
+    let random:number
 
+    do {
+      random = Math.floor(Math.random() * heroArray.length);
+    } while (lastHeroes.value.some((hero) => hero.Hero === heroArray[random].Hero));
+
+    currentHero.value = heroArray[random] 
   }
 
 </script>
 
 <template>
   <div class="min-h-screen min-w-screen bg-cover bg-[url(/background.jpg)]">
-    <div class="h-screen w-screen bg-black/50 bg-o flex flex-col gap-4 justify-center items-center">
+    <div class="h-screen w-screen bg-black/50 flex justify-center items-center">
+
+      <div class="h-[700px] flex flex-col gap-4 items-center">
+
         <button
-        class="border rounded-lg bg-white text-2xl p-2 cursor-pointer"
-        @click="randomHero()"
-        >
-        Give me a random Hero!
-      </button>
+          class="border rounded-lg bg-white text-2xl p-2 cursor-pointer"
+          @click="randomHero('')"
+          >
+          Give me a random Hero!
+        </button>
       
-      <div class="flex flex-col gap-2">
-        <p class="text-black text-2xl">Last 3 Heroes:</p>
-        <p class="text-black text-2xl" v-for="hero in lastHeroes" :key="hero.Hero">{{ hero.Hero }}</p>
-      </div>
-      
-      <div v-if="currentHero.Hero !== ''">
-        <img :src="/icons/ + currentHero.Icon" :alt="currentHero.Hero" />
+        <div class="flex gap-1">
+          <button
+            class="border rounded-lg bg-white text-xl p-2 cursor-pointer"
+            @click="randomHero('Vanguard')"
+            >
+            Random Vanguard!
+          </button>
+          <button
+          class="border rounded-lg bg-white text-xl p-2 cursor-pointer"
+          @click="randomHero('Duelist')"
+          >
+            Random Duelist!
+          </button>
+          <button
+            class="border rounded-lg bg-white text-xl p-2 cursor-pointer"
+            @click="randomHero('Strategist')"
+            >
+            Random Strategist!
+          </button>
+        </div>
+
+        
+        <div v-if="currentHero.Hero !== ''">
+          <img :src="/icons/ + currentHero.Icon" :alt="currentHero.Hero" />
+        </div>
+        
+        <div v-if="lastHeroes.length > 0" class="flex flex-col gap-2 p-2 rounded-lg bg-white/50">
+          <p class="text-black text-2xl">Last 3 Heroes:</p>
+          <p class="text-black text-xl" v-for="hero in lastHeroes" :key="hero.Hero">{{ hero.Hero }}</p>
+        </div>
+        
       </div>
     </div>
   </div>
